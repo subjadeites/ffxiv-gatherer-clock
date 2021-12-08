2c787362e8d84d1cdb3b3c9ff89d2ae42b31288c
subjadeites <sublunary95@gmail.com>
2021-12-08 21:21:18 +0800

    0.2.0 add more select adjust some code

--- clock.py
+++ clock.py
@@ -1,17 +1,15 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2021/12/7 22:33
+# @Time    : 2021/12/8 17:30
+# @Version : 0.1.1
 # @Author  : subjadeites
 # @File    : clock.py
-import time
 import datetime
+import time
+
 import pandas as pd
 import win32com.client
 from terminaltables import AsciiTable
-
-# 导入系统tts
-spk = win32com.client.Dispatch("SAPI.SpVoice")
-spk_jp = win32com.client.Dispatch("SAPI.SpVoice")
 
 
 # 计算ET
@@ -23,33 +21,11 @@ def Eozea_time() -> int:
     return eozea_hour
 
 
-# 导入采集时钟
-try:
-    clock = pd.read_csv(r".\list.csv", encoding='UTF-8-sig')
-    # select_0 = clock[clock['开始ET']==0].head()
-except:
-    print("list.csv丢失，请把压缩包全部解压后使用！")
-
-choose_dict = {
-    0: "",
-    1: "& (clock['等级'] < 90) & (clock['类型'] == '收藏品')",
-    2: "& (clock['等级'] == 90) & (clock['类型'] == '收藏品')",
-    3: "& ((clock['类型'] == '精选晓月灵砂') | (clock['类型'] == '精选巨树灵砂') | (clock['类型'] == '精选巨岩灵砂'))",
-    4: "& (clock['类型'] == '传说')",
-    5: "& (clock['类型'] == '传说1星')",
-}
-
-choose_ZhiYe_dict = {
-    0: "",
-    1: "& (clock['职能'] == '采掘')",
-    2: "& (clock['园艺'] == '采掘')",
-}
-
-
 def clock_out(eozea_time_in, need_tts, func, ZhiYe, lvl) -> int:
     out_list = [["材料名", "等级", "职能", "类型", "地区", "靠近水晶", "开始ET", "结束ET"], ]
-    select = "(clock['开始ET'] <= eozea_time_in) & (clock['结束ET'] > eozea_time_in)" + choose_dict.get(
-        func) + choose_ZhiYe_dict.get(ZhiYe) + "& (clock['等级'] <= " + str(lvl) + ")"
+    select = "(clock['开始ET'] <= eozea_time_in) & (clock['结束ET'] > eozea_time_in)" + func_select(
+        func) + choose_ZhiYe_dict.get(
+        ZhiYe) + "& (clock['等级'] <= " + str(lvl) + ")"
     clock_found = clock[eval(select)].head()
     if len(clock_found) == 0:
         if (eozea_time_in % 2) == 0:
@@ -78,8 +54,17 @@ def choose_func():
     choose_in = input("请输入你的选择：")
     try:
         choose_in = int(choose_in)
-        if choose_in in [0, 1, 2, 3, 4, 5]:
-            return choose_in
+        if choose_in == 0:
+            return [0]
+        elif choose_in == 1:
+            temp_list = choose_func_more()
+            result_list = []
+            for k in temp_list:
+                if k is None:
+                    pass
+                else:
+                    result_list.append(k)
+            return result_list
         else:
             print("非以上选项，请重新输入！")
             choose_func()
@@ -88,12 +73,50 @@ def choose_func():
         choose_func()
 
 
+def check_input(check, func_num: int):
+    if check == '1':
+        return func_num
+    else:
+        return None
+
+
+def choose_func_more() -> list:
+    result_list = []
+    choose_in = input("是否需要提醒【白票收藏品】，需要请输入1，不需要可以直接回车：")
+    result_list.append(check_input(choose_in, 1))
+    choose_in = input("是否需要提醒【紫票收藏品】，需要请输入1，不需要可以直接回车：")
+    result_list.append(check_input(choose_in, 2))
+    choose_in = input("是否需要提醒【精选灵砂】，需要请输入1，不需要可以直接回车：")
+    result_list.append(check_input(choose_in, 3))
+    choose_in = input("是否需要提醒【普通传说点】，需要请输入1，不需要可以直接回车：")
+    result_list.append(check_input(choose_in, 4))
+    choose_in = input("是否需要提醒【1星传说点】，需要请输入1，不需要可以直接回车：")
+    result_list.append(check_input(choose_in, 5))
+    # 用于处理5个None的情况，五个None会要求提建议/重新输入
+    if result_list.count(None) == 5:
+        print("未选择任何一项时限，如果是输入错误请重新选择；如果是功能缺失请去帖子/github提出建议与意见~")
+        choose_func_more()
+    else:
+        return result_list
+
+
+def func_select(func) -> str:
+    result = "& ("
+    for k in func:
+        result += (choose_dict.get(k) + "|")
+    if result[-1:] == "|":
+        result = result[:-1] + ")"
+    return result
+
+
 def choose_ZhiYe():
     choose_in = input("请输入你选择的职业：")
     try:
         choose_in = int(choose_in)
         if choose_in in [0, 1, 2]:
             return choose_in
+        elif choose_in == "":
+            return 0
         else:
             print("非以上选项，请重新输入！")
             choose_ZhiYe()
@@ -141,24 +164,52 @@ def choose_tts():
 
 
 if __name__ == '__main__':
+    print("正在初始化依赖与环境")
+
+    # 导入系统tts
+    spk = win32com.client.Dispatch("SAPI.SpVoice")
+    spk_jp = win32com.client.Dispatch("SAPI.SpVoice")
+
+    # 导入采集时钟
+    try:
+        clock = pd.read_csv(r".\list.csv", encoding='UTF-8-sig')
+        # select_0 = clock[clock['开始ET']==0].head()
+    except:
+        print("list.csv丢失，请把压缩包全部解压后使用！")
+
+    choose_dict = {
+        0: "",
+        1: " ((clock['等级'] < 90) & (clock['类型'] == '收藏品'))",
+        2: " ((clock['等级'] == 90) & (clock['类型'] == '收藏品'))",
+        3: " ((clock['类型'] == '精选晓月灵砂') | (clock['类型'] == '精选巨树灵砂') | (clock['类型'] == '精选巨岩灵砂'))",
+        4: " (clock['类型'] == '传说')",
+        5: " (clock['类型'] == '传说1星')",
+    }
+
+    choose_ZhiYe_dict = {
+        0: "",
+        1: "& (clock['职能'] == '采掘')",
+        2: "& (clock['园艺'] == '采掘')",
+    }
+
     print("欢迎使用原生态手搓纯天然本地采集时钟！")
-    print("当前程序版本：0.0.1")
+    print("当前程序版本：0.1.0")
     print("当前数据版本：国际服6.0")
-    print("请根据提示输入需要提醒的采集点种类：")
-    print("0：全部，1：白票收藏品，2：紫票收藏品，3：精选灵砂，4：普通传说点，5：1星传说点。")
+    print("请根据提示输入需要提醒的采集点种类。")
+    print("0：全部，1：自行多选")
     result_func = choose_func()
-    print("请根据提示输入需要提醒的职业：")
-    print("0：全部，1：采掘，2：园艺。")
+    print("请根据提示输入需要提醒的职业。")
+    print("0：全部，1：采掘，2：园艺，不输入默认全部。")
     result_ZhiYe = choose_ZhiYe()
-    print("请根据提示输入需要提醒多少级以下的时限（包含该等级），不输入默认或输入0则全部提醒")
+    print("请根据提示输入需要提醒多少级以下的时限（包含该等级），不输入默认或输入0则全部提醒。")
     result_lvl = choose_lvl()
-    print("请根据提示输入是否需要TTS")
+    print("请根据提示输入是否需要TTS。")
     print("0：不需要，1：需要，不输入默认为需要。")
     result_tts = choose_tts()
     last_clock_time = 0
     while True:
         now_eozea_hour = Eozea_time()
-        if now_eozea_hour == 0 and last_clock_time ==24:
+        if now_eozea_hour == 0 and last_clock_time == 24:
             last_clock_time = 0
         if now_eozea_hour >= last_clock_time:
             print("================")
@@ -166,7 +217,7 @@ if __name__ == '__main__':
             print("================")
             temp_next_clock_time = clock_out(now_eozea_hour, result_tts, result_func, result_ZhiYe, result_lvl)
             last_clock_time = temp_next_clock_time
-            if result_func ==3:
+            if result_func == 3:
                 if (last_clock_time % 4) == 0:
                     pass
                 else:
