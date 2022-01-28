4519eaaa87bb7f103d3e7393e12b10dc16cd6c1d
subjadeites <sublunary95@gmail.com>
2022-01-28 17:53:29 +0800

    1.2.1 Fix 2.x Clock Fix csv

--- main.py
+++ main.py
@@ -38,130 +38,6 @@ def Eorzea_time() -> int:
     return Eorzea_hour
 
 
-# 闹钟核心方法
-def clock_out(lang, Eorzea_time_in, need_tts, func, ZhiYe, lvl_min, lvl_max, choose_DLC, client_verion,
-              more_select_result) -> int:
-    # 换日
-    if Eorzea_time_in == 22 or Eorzea_time_in == 23:
-        next_start_time = 0
-    elif (Eorzea_time_in % 2) == 0:
-        next_start_time = Eorzea_time_in + 2
-    else:
-        next_start_time = Eorzea_time_in + 1
-    # 国服更新6.0后需修改
-    if client_verion == '国服':
-        choose_DLC = '国服全部'
-    # 当前ET刷新
-    if choose_DLC == '自定义筛选':
-        out_list = []
-        select = "(clock['开始ET'] <= Eorzea_time_in) & (clock['结束ET'] > Eorzea_time_in)" + '&clock.材料名' + lang + '.isin(more_select_result)'
-        clock_found = clock[eval(select)].sort_values(by='等级',ascending=False).head(None)
-        out_list_next = []
-        select_next = "(clock['开始ET'] <= next_start_time) & (clock['结束ET'] > next_start_time)" + '&clock.材料名' + lang + '.isin(more_select_result)'
-        clock_found_next = clock[eval(select_next)].sort_values(by='等级',ascending=False).head(None)
-    else:
-        out_list = []
-        select = "(clock['开始ET'] <= Eorzea_time_in) & (clock['结束ET'] > Eorzea_time_in)" + func_select(
-            func) + choose_ZhiYe_dict.get(
-            ZhiYe) + "& (clock['等级'] <= " + str(lvl_max) + ")" + "& (clock['等级'] >= " + str(
-            lvl_min) + ")" + choose_DLC_dict.get(choose_DLC)
-        clock_found = clock[eval(select)].sort_values(by='等级',ascending=False).head(None)
-        # 这部分是用于预告下一次刷新的代码
-        out_list_next = []
-        select_next = "(clock['开始ET'] <= next_start_time) & (clock['结束ET'] > next_start_time)" + func_select(
-            func) + choose_ZhiYe_dict.get(
-            ZhiYe) + "& (clock['等级'] <= " + str(lvl_max) + ")" + "& (clock['等级'] >= " + str(
-            lvl_min) + ")" + choose_DLC_dict.get(choose_DLC)
-        clock_found_next = clock[eval(select_next)].sort_values(by='等级',ascending=False).head(None)
-    # 如果本时段和下个时段都没有结果，那么直接返回
-    if len(clock_found) == 0 and len(clock_found_next) == 0:
-        frame.img_ctrl.Show(False)  # 关闭图片窗体显示
-        frame.out_listctrl.DeleteAllItems()
-        frame.out_listctrl.InsertItem(0, '当前时段无筛选条件下结果！')
-        frame.out_listctrl_next.DeleteAllItems()
-        frame.out_listctrl_next.InsertItem(0, '当前时段无筛选条件下结果！')
-        return next_start_time
-    else:
-        frame.img_ctrl.Show(False)  # 关闭图片窗体显示
-        place_keyword = '地区CN'  # i18n用
-        for i in range(0, len(clock_found)):
-            temp_out_list = [clock_found.iloc[i]['材料名' + lang], str(int(clock_found.iloc[i]['等级'])),
-                             clock_found.iloc[i]['职能'],
-                             clock_found.iloc[i]['类型'], clock_found.iloc[i][place_keyword], clock_found.iloc[i]['靠近水晶'],
-                             str(clock_found.iloc[i]['开始ET']), str(clock_found.iloc[i]['结束ET'])]
-            out_list.append(temp_out_list)
-        # region 这部分是用于预告下一次刷新的代码
-        for i in range(0, len(clock_found_next)):
-            temp_out_list_2 = [clock_found_next.iloc[i]['材料名' + lang], str(int(clock_found_next.iloc[i]['等级'])),
-                               clock_found_next.iloc[i]['职能'],
-                               clock_found_next.iloc[i]['类型'], clock_found_next.iloc[i][place_keyword],
-                               clock_found_next.iloc[i]['靠近水晶'],
-                               str(clock_found_next.iloc[i]['开始ET']), str(clock_found_next.iloc[i]['结束ET'])]
-            out_list_next.append(temp_out_list_2)
-        # endregion
-        # 格式化输出
-        if len(clock_found) == 0:
-            frame.out_listctrl.DeleteAllItems()
-            frame.out_listctrl_next.DeleteAllItems()
-            frame.out_listctrl.InsertItem(0, '当前时段无筛选条件下结果！')
-            i = 0
-            for v in out_list_next:
-                index = frame.out_listctrl_next.InsertItem(i, v[0])
-                for num_i in range(1, 8):
-                    if pd.isnull(v[num_i]):
-                        v[num_i] = "无数据"
-                    frame.out_listctrl_next.SetItem(index, num_i, v[num_i])
-                i += 1
-        elif len(clock_found_next) == 0:
-            frame.out_listctrl.DeleteAllItems()
-            frame.out_listctrl_next.DeleteAllItems()
-            i = 0
-            for v in out_list:
-                index = frame.out_listctrl.InsertItem(i, v[0])
-                for num_i in range(1, 8):
-                    if pd.isnull(v[num_i]):
-                        v[num_i] = "无数据"
-                    frame.out_listctrl.SetItem(index, num_i, v[num_i])
-                i += 1
-            frame.out_listctrl_next.InsertItem(0, '当前时段无筛选条件下结果！')
-        else:
-            frame.out_listctrl.DeleteAllItems()
-            frame.out_listctrl_next.DeleteAllItems()
-            i = 0
-            for v in out_list:
-                index = frame.out_listctrl.InsertItem(i, v[0])
-                for num_i in range(1, 8):
-                    if pd.isnull(v[num_i]):
-                        v[num_i] = "无数据"
-                    frame.out_listctrl.SetItem(index, num_i, v[num_i])
-                i += 1
-            i = 0
-            for v in out_list_next:
-                index = frame.out_listctrl_next.InsertItem(i, v[0])
-                for num_i in range(1, 8):
-                    if pd.isnull(v[num_i]):
-                        v[num_i] = "无数据"
-                    frame.out_listctrl_next.SetItem(index, num_i, v[num_i])
-                i += 1
-
-        if need_tts is True:
-            tts_word = ""
-            for i in range(0, len(out_list)):
-                nearby = '' if out_list[i][5] == '无数据' else out_list[i][5]
-                tts_word = tts_word + out_list[i][0] + "。"
-                tts_word = tts_word + str(out_list[i][1]) + "级" + "。"
-                tts_word = tts_word + out_list[i][2] + "。"
-                tts_word = tts_word + out_list[i][3] + "。"
-                tts_word = tts_word + out_list[i][4] + "。"
-                tts_word = tts_word + nearby + "。"
-            if len(clock_found_next) > 0:
-                tts_word = tts_word + "已为您更新下个时段预告" + "。"
-            elif len(clock_found_next) == 0:
-                tts_word = tts_word + "下个时段无筛选条件下结果" + "。"
-            tts(tts_word)
-        return next_start_time
-
-
 # 组合筛选条件
 def func_select(func) -> str:
     if func == [0]:
@@ -173,6 +49,147 @@ def func_select(func) -> str:
         if result[-1:] == "|":
             result = result[:-1] + ")"
         return result
+
+
+# 闹钟核心方法
+def clock_out(lang, Eorzea_time_in, need_tts, func, ZhiYe, lvl_min, lvl_max, choose_DLC, client_verion,
+              more_select_result) -> int:
+    # 换日,兼容2.0里3个ET小时刷新的时限
+    if Eorzea_time_in == 23:
+        next_start_time = 0
+    else:
+        next_start_time = Eorzea_time_in + 1
+    # 国服更新6.0后需修改
+    if client_verion == '国服':
+        choose_DLC = '国服全部'
+
+    # 筛选用字段准备
+    time_select = "(clock['开始ET'] <= Eorzea_time_in) & (clock['结束ET'] > Eorzea_time_in)"
+    time_select_next = "(clock['开始ET'] <= next_start_time) & (clock['结束ET'] > next_start_time)"
+    lvl_select = "& (clock['等级'] <= " + str(lvl_max) + ")" + "& (clock['等级'] >= " + str(lvl_min) + ")"
+    DLC_select = choose_DLC_dict.get(choose_DLC)
+    ZhiYe_select = choose_ZhiYe_dict.get(ZhiYe)
+    # 筛选符合条件的时限
+    if choose_DLC == '自定义筛选':
+        out_list = []
+        select = time_select + '&clock.材料名' + lang + '.isin(more_select_result)'
+        clock_found = clock[eval(select)].sort_values(by='等级', ascending=False).head(None)
+        out_list_next = []
+        select_next = time_select_next + '&clock.材料名' + lang + '.isin(more_select_result)'
+        clock_found_next = clock[eval(select_next)].sort_values(by='等级', ascending=False).head(None)
+    else:  # DLC筛选模式
+        out_list = []
+        select = time_select + func_select(func) + ZhiYe_select + lvl_select + DLC_select
+        clock_found = clock[eval(select)].sort_values(by='等级', ascending=False).head(None)
+        # 这部分是用于预告下一次刷新的代码
+        out_list_next = []
+        select_next = time_select_next + func_select(func) + ZhiYe_select + lvl_select + DLC_select
+        clock_found_next = clock[eval(select_next)].sort_values(by='等级', ascending=False).head(None)
+
+    old_out_list = []
+    for i in range(0, frame.out_listctrl.GetItemCount()):
+        old_out_list.append(frame.out_listctrl.GetItemText(i, 0))
+
+    frame.out_listctrl.DeleteAllItems()
+    frame.out_listctrl_next.DeleteAllItems()
+    # 如果本时段和下个时段都没有结果，那么直接返回
+    if len(clock_found) == 0 and len(clock_found_next) == 0:
+        frame.img_ctrl.Show(False)  # 关闭图片窗体显示
+        frame.out_listctrl.InsertItem(0, '当前时段无筛选条件下结果！')
+        frame.out_listctrl_next.InsertItem(0, '当前时段无筛选条件下结果！')
+        return next_start_time
+    else:
+        frame.img_ctrl.Show(False)  # 关闭图片窗体显示
+        place_keyword = '地区CN'  # i18n用
+        for i in range(0, len(clock_found)):
+            temp_i_result = clock_found.iloc[i]
+            temp_out_list = [temp_i_result['材料名' + lang], str(int(temp_i_result['等级'])), temp_i_result['职能'],
+                             temp_i_result['类型'], temp_i_result[place_keyword], temp_i_result['靠近水晶'],
+                             str(temp_i_result['开始ET']), str(temp_i_result['结束ET'])]
+            out_list.append(temp_out_list)
+        # region 这部分是用于预告下一次刷新的代码
+        for i in range(0, len(clock_found_next)):
+            temp_i_result = clock_found_next.iloc[i]
+            temp_out_list_2 = [temp_i_result['材料名' + lang], str(int(temp_i_result['等级'])), temp_i_result['职能'],
+                               temp_i_result['类型'], temp_i_result[place_keyword], temp_i_result['靠近水晶'],
+                               str(temp_i_result['开始ET']), str(temp_i_result['结束ET'])]
+            out_list_next.append(temp_out_list_2)
+        # endregion
+        # 格式化输出
+        if len(clock_found) == 0:
+            frame.out_listctrl.InsertItem(0, '当前时段无筛选条件下结果！')
+            i = 0
+            for v in out_list_next:
+                index = frame.out_listctrl_next.InsertItem(i, v[0])
+                for num_i in range(1, 8):
+                    if pd.isnull(v[num_i]) and num_i == 5:
+                        v[num_i] = ""
+                    elif pd.isnull(v[num_i]):
+                        v[num_i] = "暂无数据"
+                    frame.out_listctrl_next.SetItem(index, num_i, v[num_i])
+                i += 1
+        elif len(clock_found_next) == 0:
+            i = 0
+            for v in out_list:
+                index = frame.out_listctrl.InsertItem(i, v[0])
+                for num_i in range(1, 8):
+                    if pd.isnull(v[num_i]) and num_i == 5:
+                        v[num_i] = ""
+                    elif pd.isnull(v[num_i]):
+                        v[num_i] = "暂无数据"
+                    frame.out_listctrl.SetItem(index, num_i, v[num_i])
+                i += 1
+            frame.out_listctrl_next.InsertItem(0, '当前时段无筛选条件下结果！')
+        else:
+            i = 0
+            for v in out_list:
+                index = frame.out_listctrl.InsertItem(i, v[0])
+                for num_i in range(1, 8):
+                    if pd.isnull(v[num_i]) and num_i == 5:
+                        v[num_i] = ""
+                    elif pd.isnull(v[num_i]):
+                        v[num_i] = "暂无数据"
+                    frame.out_listctrl.SetItem(index, num_i, v[num_i])
+                i += 1
+            i = 0
+            for v in out_list_next:
+                index = frame.out_listctrl_next.InsertItem(i, v[0])
+                for num_i in range(1, 8):
+                    if pd.isnull(v[num_i]) and num_i == 5:
+                        v[num_i] = ""
+                    elif pd.isnull(v[num_i]):
+                        v[num_i] = "暂无数据"
+                    frame.out_listctrl_next.SetItem(index, num_i, v[num_i])
+                i += 1
+
+        if need_tts is True:
+            tts_word = ""
+            for i in range(0, len(out_list)):
+                if out_list[i][0] in old_out_list:
+                    pass
+                else:
+                    nearby = '' if out_list[i][5] == '暂无数据' else out_list[i][5]
+                    tts_word = tts_word + out_list[i][0] + "。"
+                    # tts_word = tts_word + str(out_list[i][1]) + "级" + "。" TODO：后续看需求加入自定义tts
+                    tts_word = tts_word + out_list[i][2] + "。"
+                    tts_word = tts_word + out_list[i][3] + "。"
+                    tts_word = tts_word + out_list[i][4] + "。"
+                    tts_word = tts_word + nearby + "。"
+            if len(clock_found_next) > 0 and out_list != out_list_next:
+                tts_word = tts_word + "已为您更新下个时段预告" + "。"
+            elif len(clock_found_next) == 0:
+                tts_word = tts_word + "下个时段无筛选条件下结果" + "。"
+            tts(tts_word)
+        else:
+            should_tss = False
+            for i in range(0, len(out_list)):
+                if out_list[i][0] not in old_out_list:
+                    should_tss = True
+                elif old_out_list == []:  # 用于首次启动闹钟时触发tts
+                    should_tss = True
+            if should_tss is True:
+                spk.Speak("时限已刷新！")
+        return next_start_time
 
 
 class Clock_Thread(Thread):
@@ -209,15 +226,6 @@ class Clock_Thread(Thread):
                     next_clock_time = clock_out(self.lang, now_Eorzea_hour, self.need_tts, self.func, self.ZhiYe,
                                                 self.lvl_min, self.lvl_max, self.choose_DLC, self.client_version,
                                                 self.more_select_result)
-                    if (len(self.func) == 1 and 3 in self.func) or self.need_tts is True:
-                        if (next_clock_time % 4) != 0:
-                            pass
-                        elif self.need_tts is True:
-                            pass
-                        else:
-                            spk.Speak("时限已刷新！")
-                    else:
-                        spk.Speak("时限已刷新！")
                 time.sleep(1)
 
     def set_values(self, lang: str, need_tts: bool, func: list, ZhiYe: int, lvl_min: int,
@@ -799,7 +807,7 @@ class More_Choose_Windows(wx.Frame):
             self.choose_DLC_4.SetValue(True)
             self.choose_DLC_5.SetValue(True)
             self.event_choose_DLC(None)
-        if self.lang =='CN':
+        if self.lang == 'CN':
             self.choose_DLC_1.SetValue(False)
             self.choose_DLC_1.Disable()
 
@@ -867,6 +875,7 @@ class More_Choose_Windows(wx.Frame):
         frame.button_run.Enable()
         self.Destroy()
 
+
 if __name__ == '__main__':
     # 启动窗口
     app = wx.App(False)
@@ -895,7 +904,7 @@ if __name__ == '__main__':
         1: " (clock['类型'] == '白票收藏品')",
         2: " (clock['类型'] == '紫票收藏品')",
         3: " clock.类型.isin(LingSha_list)",
-        4: " (clock['类型'] == '传说')",
+        4: " (clock['类型'].str.contains('传说'))",
         5: " (clock['类型'] == '传说1星')",
         6: " (clock['类型'] == '水晶')",
         7: " (clock['类型'] == '晶簇')",
