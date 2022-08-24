387e4da6f6698ce98cf012bd079ddea28d58386f
subjadeites <sublunary95@gmail.com>
2022-08-24 15:18:52 +0800

    1.4.7 国际服6.2 fix csv bug

--- lib/update.py
+++ lib/update.py
@@ -10,7 +10,7 @@ from threading import Thread
 import requests
 import wx
 
-version = "1.4.6"
+version = "1.4.7"
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
 
 
